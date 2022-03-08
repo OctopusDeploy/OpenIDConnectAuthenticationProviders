f3011c5a0b217321c6bddd5b0494a17560d1e872
Frank Lin <frank.lin@octopus.com>
2022-03-08 22:37:44 +1000

    Restore order

--- source/Server.OctopusID/Configuration/OctopusIDConfigurationResource.cs
+++ source/Server.OctopusID/Configuration/OctopusIDConfigurationResource.cs
@@ -11,7 +11,20 @@ namespace Octopus.Server.Extensibility.Authentication.OctopusID.Configuration
         [ReadOnly(true)]
         public override string? Issuer { get; set; }
 
+        /// <summary>
+        /// NOTE: the following properties are here to control the order they appear on the settings page
+        /// </summary>
+
+        [Writeable]
+        public override string? ClientId { get; set; }
+
+        [Writeable]
+        public override SensitiveValue? ClientSecret { get; set; }
+
         [ReadOnly(true)]
         public override bool? AllowAutoUserCreation { get; set; }
+
+        [ReadOnly(true)]
+        public new bool IsEnabled { get; set; }
     }
 }

4719713523a1088c02b9b91464b5dc260afce9a8
Nelson Susanto <nelson.susanto@octopus.com>
2022-03-17 09:19:48 +1100

    First version of code verifier and code challenge

---
+++ source/Server.OpenIDConnect.Common/Infrastructure/CodeChallenge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Infrastructure
+{
+    public static class CodeChallenge
+    {
+        public static string CreateS256CodeChallenge(string codeVerifier)
+        {
+            using var sha = SHA256.Create();
+            var codeChallenge = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(codeVerifier)))
+                .TrimEnd('=').Replace("/", string.Empty).Replace("+", string.Empty);
+            return codeChallenge;
+        }
+    }
+}
---
+++ source/Server.OpenIDConnect.Common/Infrastructure/CodeVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Infrastructure
+{
+    public static class CodeVerifier
+    {
+        static readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
+
+        public static string GenerateUrlSafeCodeVerifier()
+        {
+            var data = new byte[32];
+            Rng.GetBytes(data);
+            var codeVerifier = Convert.ToBase64String(data).TrimEnd('=').Replace("/", string.Empty).Replace("+", string.Empty);
+            return codeVerifier;
+        }
+    }
+}

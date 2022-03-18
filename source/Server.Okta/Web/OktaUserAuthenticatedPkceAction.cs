5df8a3726dc4608a3cd8ec6bb5e888b5b3d4834d
Nelson Susanto <nelson.susanto@octopus.com>
2022-03-18 16:33:36 +1100

    Updates for Okta

---
+++ source/Server.Okta/Web/OktaUserAuthenticatedPkceAction.cs
@@ -0,0 +1,34 @@
+﻿using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.HostServices;
+using Octopus.Server.Extensibility.Authentication.Okta.Configuration;
+using Octopus.Server.Extensibility.Authentication.Okta.Identities;
+using Octopus.Server.Extensibility.Authentication.Okta.Tokens;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Infrastructure;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Issuer;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Web;
+using Octopus.Server.Extensibility.HostServices.Web;
+using Octopus.Time;
+
+namespace Octopus.Server.Extensibility.Authentication.Okta.Web
+{
+    class OktaUserAuthenticatedPkceAction
+        : UserAuthenticatedPkceAction<IOktaConfigurationStore, IOktaAuthTokenHandler, IOktaIdentityCreator>
+    {
+        public OktaUserAuthenticatedPkceAction(
+            ISystemLog log,
+            IOktaAuthTokenHandler authTokenHandler,
+            IPrincipalToUserResourceMapper principalToUserResourceMapper,
+            IUpdateableUserStore userStore,
+            IOktaConfigurationStore configurationStore,
+            IAuthCookieCreator authCookieCreator,
+            IInvalidLoginTracker loginTracker,
+            ISleep sleep,
+            IOktaIdentityCreator identityCreator,
+            IClock clock, IUrlEncoder encoder, IIdentityProviderConfigDiscoverer identityProviderConfigDiscoverer)
+            : base(log, authTokenHandler, principalToUserResourceMapper, userStore, configurationStore, authCookieCreator, loginTracker, sleep, identityCreator, clock, encoder, identityProviderConfigDiscoverer)
+        {
+        }
+
+        protected override string ProviderName => OktaAuthenticationProvider.ProviderName;
+    }
+}

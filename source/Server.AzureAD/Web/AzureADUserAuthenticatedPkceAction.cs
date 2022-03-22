a415e81c7de7a98d9f9d08ee126df6193a79b70c
Nelson Susanto <nelson.susanto@octopus.com>
2022-03-22 14:38:29 +1100

    Add actions

---
+++ source/Server.AzureAD/Web/AzureADUserAuthenticatedPkceAction.cs
@@ -0,0 +1,47 @@
+﻿using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Configuration;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Identities;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Infrastructure;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Tokens;
+using Octopus.Server.Extensibility.Authentication.HostServices;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Issuer;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Web;
+using Octopus.Server.Extensibility.HostServices.Web;
+using Octopus.Time;
+
+namespace Octopus.Server.Extensibility.Authentication.AzureAD.Web
+{
+    class AzureADUserAuthenticatedPkceAction : UserAuthenticatedPkceAction<IAzureADConfigurationStore, IAzureADAuthTokenHandler, IAzureADIdentityCreator>
+    {
+        public AzureADUserAuthenticatedPkceAction(
+            ISystemLog log,
+            IAzureADAuthTokenHandler authTokenHandler,
+            IAzureADPrincipalToUserResourceMapper principalToUserResourceMapper,
+            IUpdateableUserStore userStore,
+            IAzureADConfigurationStore configurationStore,
+            IAuthCookieCreator authCookieCreator,
+            IInvalidLoginTracker loginTracker,
+            ISleep sleep,
+            IAzureADIdentityCreator identityCreator,
+            IClock clock,
+            IUrlEncoder encoder,
+            IIdentityProviderConfigDiscoverer identityProviderConfigDiscoverer) :
+            base(
+                log,
+                authTokenHandler,
+                principalToUserResourceMapper,
+                userStore,
+                configurationStore,
+                authCookieCreator,
+                loginTracker,
+                sleep,
+                identityCreator,
+                clock,
+                encoder,
+                identityProviderConfigDiscoverer)
+        {
+        }
+
+        protected override string ProviderName => AzureADAuthenticationProvider.ProviderName;
+    }
+}
---
+++ source/Server.OctopusID/Web/OctopusIDUserAuthenticatedPkceAction.cs
@@ -0,0 +1,34 @@
+﻿using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.HostServices;
+using Octopus.Server.Extensibility.Authentication.OctopusID.Configuration;
+using Octopus.Server.Extensibility.Authentication.OctopusID.Identities;
+using Octopus.Server.Extensibility.Authentication.OctopusID.Tokens;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Infrastructure;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Issuer;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Web;
+using Octopus.Server.Extensibility.HostServices.Web;
+using Octopus.Time;
+
+namespace Octopus.Server.Extensibility.Authentication.OctopusID.Web
+{
+    class OctopusIDUserAuthenticatedPkceAction
+        : UserAuthenticatedPkceAction<IOctopusIDConfigurationStore, IOctopusIDAuthTokenHandler, IOctopusIDIdentityCreator>
+    {
+        public OctopusIDUserAuthenticatedPkceAction(
+            ISystemLog log,
+            IOctopusIDAuthTokenHandler authTokenHandler,
+            IPrincipalToUserResourceMapper principalToUserResourceMapper,
+            IUpdateableUserStore userStore,
+            IOctopusIDConfigurationStore configurationStore,
+            IAuthCookieCreator authCookieCreator,
+            IInvalidLoginTracker loginTracker,
+            ISleep sleep,
+            IOctopusIDIdentityCreator identityCreator,
+            IClock clock, IUrlEncoder encoder, IIdentityProviderConfigDiscoverer identityProviderConfigDiscoverer)
+            : base(log, authTokenHandler, principalToUserResourceMapper, userStore, configurationStore, authCookieCreator, loginTracker, sleep, identityCreator, clock, encoder, identityProviderConfigDiscoverer)
+        {
+        }
+
+        protected override string ProviderName => OctopusIDAuthenticationProvider.ProviderName;
+    }
+}
